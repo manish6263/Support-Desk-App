19a2046ab335fba0f57247423ea71a3328d8029c
Manish <mp626329@gmail.com>
2022-12-10 14:53:00 +0530

    toast error removed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,6 @@ import { toast } from 'react-toastify';
 import { FaSignInAlt } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import Spinner from '../components/Spinner';
 import { login, reset } from '../features/auth/authSlice';
 
