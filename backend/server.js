7c7e9314a6ad3c19b3debe9d4004260e70577caf
Manish <mp626329@gmail.com>
2022-12-10 10:33:37 +0530

    proxy error removed

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 require('colors');
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Connected to Database
